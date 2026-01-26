63e7e7f8e440d9fb11a5ac84162e4d52908c9245
Angela Trentin <angela.trentin@mat.tn.it>
2026-01-26 13:29:31 +0100

    feat: creato toggle dark mode

---
+++ afp-his/src/app/darkmode-selector/darkmode-selector.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'his-darkmode-selector',
+  imports: [ButtonModule],
+  templateUrl: './darkmode-selector.html',
+  styleUrl: './darkmode-selector.scss',
+})
+export class DarkmodeSelector {
+  toggleDarkMode(){
+    const element = document.querySelector('html');
+    element?.classList.toggle('my-app-dark');
+  }
+
+}
