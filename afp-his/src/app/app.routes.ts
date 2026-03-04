a13b51439194fefcd3cc36fae38c10d941dd633c
angelatrentin <angela.trentin@mat.tn.it>
2026-03-04 14:23:53 +0100

    feat: add new components for patient management and update routing

--- afp-his/src/app/app.routes.ts
+++ afp-his/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { ListaPz } from './pattern/lista-pz/lista-pz';
+import { AccettazionePz } from './fetures/accettazione-pz/accettazione-pz';
+import { StatoServizi } from './fetures/stato-servizi/stato-servizi';
+import { Component } from '@angular/core';
+import { ModificaPz } from './fetures/modifica-pz/modifica-pz';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //ORDINE DELLE ROTTE NON è CASUALE
+    //1.rotte popolate
+    {
+        path: 'lista-pz',
+        component: ListaPz,
+    },
+    {
+        path: 'accettazione-pz',
+        component: AccettazionePz,
+    },
+    {
+        path: 'modifica-pz:id',
+        component: ModificaPz,
+    },
+    {
+        path: 'stato-servizi',
+        component: StatoServizi,
+    },
+
+
+    //2.rotta vuota
+    {
+        path: '',
+        redirectTo: 'lista-pz',
+        pathMatch: 'full',
+    },
+
+    //3.rotta per qualsiasi altro caso
+    {
+        path: '**',
+        redirectTo: 'lista-pz',
+        pathMatch: 'full',
+    }
+];
---
+++ afp-his/src/app/fetures/lista-pz/lista-pz.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'his-lista-pz',
+  imports: [],
+  templateUrl: './lista-pz.html',
+  styleUrl: './lista-pz.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListaPz {
+
+}
