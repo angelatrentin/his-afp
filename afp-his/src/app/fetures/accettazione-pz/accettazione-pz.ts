5c1809dd6f97c40c1f26965e74c5f7355f3d4daa
angelatrentin <angela.trentin@mat.tn.it>
2026-03-23 12:20:04 +0100

    feat: enhance accettazione-pz component with form validation and error handling for nome and cognome fields

--- afp-his/src/app/fetures/accettazione-pz/accettazione-pz.ts
+++ afp-his/src/app/fetures/accettazione-pz/accettazione-pz.ts
@@ -2,21 +2,53 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GestioneRisorse } from '../../core/risorse/gestione-risorse';
 import { JsonPipe } from '@angular/common';
 import { InputText } from "primeng/inputtext";
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'his-accettazione-pz',
-  imports: [JsonPipe, InputText, ReactiveFormsModule],
+  imports: [JsonPipe, InputText, ReactiveFormsModule, Button],
   templateUrl: './accettazione-pz.html',
   styleUrl: './accettazione-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccettazionePz {
   GestioneRisorse = inject(GestioneRisorse);
-  nome = new FormControl('',
-    [Validators.required,
-      Validators.minLength(2),
-      Validators.maxLength(30),
-    
-    ]);
+
+  paziente = new FormGroup ({
+    nome: new FormControl('', [Validators.required]),
+    cognome: new FormControl('', [Validators.required]),
+    residenza: new FormGroup ({
+      via: new FormControl(),
+      civico: new FormControl(),
+    })
+  });
+  checkFormControl(control: string) {
+    const fc = this.paziente.get(control);
+    return fc?.invalid && (fc.touched || fc.dirty);
+
+    // si può usare anche scritto così
+    // if (fc) {
+    //   fc?.invalid && (fc?.touched || fc?.dirty);
+    // }
+    // return null;
+  }
+
+  checkFormControlError(control: string, error: string) {
+    const fc = this.paziente.get(control);
+
+    if(fc && fc.hasError(error)) {
+      return fc.getError(error);
+    } else {
+      return null;
+    }
+  }
+
+  onSubmit() {
+    if (this.paziente.valid) {
+      console.log(this.paziente.value);
+    }else{
+      this.paziente.markAllAsTouched();
+    }
+  }
 }
