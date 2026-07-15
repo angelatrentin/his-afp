c992a774a414cc296629e0cb2d42251f49cb95d0
Angela Trentin <angela.trentin@mat.tn.it>
2026-07-15 06:39:43 +0000

    feat(accettazione-pz): update route to use AccettazioneWrapperComponent

--- his-afp/src/app/app.routes.ts
+++ his-afp/src/app/app.routes.ts
@@ -6,9 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./features/lista-pz/lista-pz').then((m) => m.ListaPz),
   },
   {
-    path: 'accettazione-pz',
-    loadComponent: () =>
-      import('./features/accettazione-pz/accettazione-pz').then((m) => m.AccettazionePz),
+  path: 'accettazione-pz',
+  loadComponent: () =>
+    import('./features/accettazione-pz/pages/accettazione-wrapper/accettazione-wrapper')
+      .then((m) => m.AccettazioneWrapperComponent),
   },
   // {
   //   path: 'modifica-pz',
